da8e37320d5c55eb1f4cb035a53203aa89af94dd
vaalu <mohu_mca@yahoo.co.in>
2020-04-02 19:58:07 +0530

    fix: Added security layer

--- alphaoneui/src/index.tsx
+++ alphaoneui/src/index.tsx
@@ -15,25 +15,15 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-import React from "react";
-import ReactDOM from "react-dom";
+import React from "react"
+import ReactDOM from "react-dom"
 
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./assets/css/animate.min.css";
-import "./assets/sass/light-bootstrap-dashboard-react.scss?v=1.3.0";
-import "./assets/css/demo.css";
-import "./assets/css/pe-icon-7-stroke.css";
-
-import AdminLayout from "layouts/Admin.jsx";
+import { BrowserRouter } from "react-router-dom"
+import Secured from './components/Secured'
 
 ReactDOM.render(
   <BrowserRouter>
-    <Switch>
-      <Route path="/admin" render={props => <AdminLayout {...props} />} />
-      <Redirect from="/" to="/admin/dashboard" />
-    </Switch>
+    <Secured />
   </BrowserRouter>,
   document.getElementById("root")
 );
