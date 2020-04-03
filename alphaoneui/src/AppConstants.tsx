ae4672e63376987b1367aa6e2b49f29148f39bab
vaalu <mohu_mca@yahoo.co.in>
2020-04-03 09:37:11 +0530

    fix: Updated for build properties

--- alphaoneui/src/AppConstants.tsx
+++ alphaoneui/src/AppConstants.tsx
@@ -1,9 +1,10 @@
 import keycloak from 'keycloak-js'
+import kc from './keycloak.json'
 
 export const KEYCLOAK_JSON = keycloak({
-	realm:'AlphaPrime', 
-	clientId:'alphaprimeui', 
-	url:'http://localhost:8080/auth' 
+	realm:kc['realm'], 
+	clientId:kc['resource'], 
+	url:kc['auth-server-url']
 })
 export const APOLLO_SERVER = 'http://localhost:4000'
 export const KAFKA_SERVER = 'localhost:9092'
