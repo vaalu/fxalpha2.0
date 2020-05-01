a56869b5fc309f769877da6cecdbb5bb44e20d59
vaalu <mohu_mca@yahoo.co.in>
2020-05-01 17:51:36 +0530

    feat: Added a sample file

--- AlicePy/src/modules/tests/Test03FileOperations.py
+++ AlicePy/src/modules/tests/Test03FileOperations.py
@@ -9,10 +9,10 @@ async def consumer_handler(websocket) -> None:
 	async for message in websocket:
 		print(message)
 async def consume(hostname: str):
-	websocket_resource_url = f"wss://ant.aliceblueonline.com/hydrasocket/v2/websocket/pKbXDMubx1UEhg4__ufy2_YOvf0iOOvjIjrlQkc0950.VGBPVlWTJn3td7KS1D5ErhuhyAQ5ter2gOPzYeIExLY"
+	websocket_resource_url = f"wss://ant.aliceblueonline.com/hydrasocket/v2/websocket/pKbXDMubx"
 	async with websockets.connect(websocket_resource_url) as websocket:
 		await consumer_handler(websocket)
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(consume('wss://ant.aliceblueonline.com/hydrasocket/v2/websocket/pKbXDMubx1UEhg4__ufy2_YOvf0iOOvjIjrlQkc0950.VGBPVlWTJn3td7KS1D5ErhuhyAQ5ter2gOPzYeIExLY'))
+loop.run_until_complete(consume('wss://ant.aliceblueonline.com/hydrasocket/v2/websocket/pKbXDM'))
 loop.run_forever()
