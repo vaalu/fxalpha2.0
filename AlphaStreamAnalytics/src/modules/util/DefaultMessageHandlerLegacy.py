008b7e70d3a535e6f505426110aa5e3683de98ea
vaalu <mohu_mca@yahoo.co.in>
2020-06-04 02:42:52 +0530

    fix: Updates to fix time issue

--- AlphaStreamAnalytics/src/modules/util/DefaultMessageHandlerLegacy.py
+++ AlphaStreamAnalytics/src/modules/util/DefaultMessageHandlerLegacy.py
@@ -2,16 +2,22 @@ import json
 import redis
 import time
 import datetime
+from dateutil import tz
 from modules.props.ConfigProps import AppLogger
+from modules.util.SingleInstanceUtil import SingleInstanceUtil
 
 logger = AppLogger('DefaultMessageHandlerLegacy')
 
 class DefaultMessageHandlerLegacy():
 	__red = redis.Redis(host='localhost', port=6379)
-	
+	__start = 0
+	__end = 0
+	__offset = 0
 	def __init__(self):
 		print('Initializing by default')
-	
+		self.__start, self.__end, self.__offset = SingleInstanceUtil().today_timings()
+	def __now_time(self):
+		return time.mktime(datetime.datetime.now().timetuple()) - self.__tz_offset()
 	def __getCache(self):
 		return self.__red;
 	
@@ -21,13 +27,14 @@ class DefaultMessageHandlerLegacy():
 		# Read value from message and store in cache for further processing
 		source_val = message
 		# Process message
-		curr_ts = float(source_val["exchange_timestamp"]) + ((datetime.datetime.now().microsecond % 100000)/100000)
-		hset_key = '%s:%s:%s'%(source_val["instrument_token"], source_val["exchange_timestamp"], curr_ts)
-		red.hset(hset_key, "exchange_timestamp", source_val["exchange_timestamp"])
+		etstamp = source_val["exchange_timestamp"] if self.__start <= int(source_val["exchange_timestamp"]) <= self.__end else self.__now_time() 
+		curr_ts = float(etstamp) + ((datetime.datetime.now().microsecond % 100000)/100000)
+		hset_key = '%s:%s:%s'%(source_val["instrument_token"], etstamp, curr_ts)
+		red.hset(hset_key, "exchange_timestamp", etstamp)
 		red.hset(hset_key, "instrument_token", source_val["instrument_token"])
 		red.hset(hset_key, "last_traded_price", source_val["last_traded_price"])
 		red.hset(hset_key, "last_traded_time", source_val["last_traded_time"])
-		logger.info('%s : %s : %s'%(hset_key, source_val["exchange_timestamp"], source_val["last_traded_price"]))
+		logger.info('%s : %s : %s'%(hset_key, etstamp, source_val["last_traded_price"]))
 		red.hset(hset_key, "last_traded_quantity", source_val["last_traded_quantity"])
 		red.hset(hset_key, "trade_volume", source_val["trade_volume"])
 		red.hset(hset_key, "best_bid_price", source_val["best_bid_price"])
@@ -37,7 +44,7 @@ class DefaultMessageHandlerLegacy():
 		red.hset(hset_key, "total_buy_quantity", source_val["total_buy_quantity"])
 		red.hset(hset_key, "total_sell_quantity", source_val["total_sell_quantity"])
 		red.hset(hset_key, "average_trade_price", source_val["average_trade_price"])
-		red.hset(hset_key, "exchange_timestamp", source_val["exchange_timestamp"])
+		red.hset(hset_key, "exchange_timestamp", etstamp)
 		red.hset(hset_key, "open_price", source_val["open_price"])
 		red.hset(hset_key, "high_price", source_val["high_price"])
 		red.hset(hset_key, "low_price", source_val["low_price"])
--- AlphaStreamAnalytics/src/modules/util/SingleInstanceUtil.py
+++ AlphaStreamAnalytics/src/modules/util/SingleInstanceUtil.py
@@ -1,3 +1,7 @@
+from datetime import datetime, tzinfo
+import pytz
+import time
+from dateutil import tz
 from modules.props.ConfigProps import AppLogger, AppProps
 from modules.util.AliceInstrumentsUtil import AliceInstruments
 
@@ -6,5 +10,18 @@ class SingleInstanceUtil():
 	__alice_instruments = AliceInstruments()
 	def __init__(self):
 		logger.info('Initializing singleton instances for the application')
+	def __tz_offset(self):
+		# Offset difference
+		return datetime.now(pytz.timezone('Asia/Kolkata')).utcoffset().total_seconds() + 5
+		# return 0
+	def __now(self):
+		return datetime.now().astimezone(tz.gettz('Asia/Kolkata')).replace(second=0)
+	def __now_time(self):
+		return time.mktime(self.__now().replace(second=0).timetuple()) - self.__tz_offset()
+	def today_timings(self):
+		today_date = self.__now()
+		curr_start_time = time.mktime(datetime(today_date.year, today_date.month, today_date.day, 9, 0, 0).timetuple()) -self.__tz_offset()
+		curr_end_time = time.mktime(datetime(today_date.year, today_date.month, today_date.day, 23, 30, 0).timetuple()) -self.__tz_offset()
+		return curr_start_time, curr_end_time, self.__tz_offset()
 	def get_instrument_util(self):
 		return self.__alice_instruments
